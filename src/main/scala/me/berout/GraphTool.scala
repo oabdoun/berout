f0fffa40b7c1bba1dc6b2293924bee1c51cd2cf4
Olivier Abdoun <aboud@maels-air.speedport_w_724v_09011603_05_017>
2018-11-01 15:30:53 +0100

    shortest path refactored to prepare for graph traversal

--- src/main/scala/me/berout/GraphTool.scala
+++ src/main/scala/me/berout/GraphTool.scala
@@ -2,23 +2,29 @@ package me.berout
 
 import scala.collection.mutable.HashMap
 
+case class Path(path: Seq[Edge], cost: Int) {}
+
 object GraphTool {
+  private val emptyPath = new Path(Seq.empty, 0)
+
   def shortestPath(graph: Graph, start: String, target: String): Seq[Edge] = {
     val costs = HashMap(start -> 0)
-    val paths: HashMap[String, Seq[Edge]] = HashMap(start -> Seq.empty)
+    val paths: HashMap[String, Path] = HashMap(start -> new Path(Seq.empty, 0))
     var waypoints = List(start)
     while (!waypoints.isEmpty) {
       for (next <- graph.outEdges(waypoints.head)) {
-        if (!costs.isDefinedAt(next.destination)
-            || (costs(next.destination) > costs(next.source) + next.weight)) {
-          costs += (next.destination -> (costs(next.source) + next.weight))
-          paths += (next.destination -> (paths(next.source) :+ next))
+        if (!paths.isDefinedAt(next.destination)
+            || (paths(next.destination).cost > paths(next.source).cost + next.weight)) {
+          paths += (next.destination -> new Path(
+            paths(next.source).path :+ next,
+            paths(next.source).cost + next.weight
+          ))
           waypoints = waypoints :+ next.destination
         }
       }
       waypoints = waypoints.tail
     }
-    paths.getOrElse(target, Seq.empty[Edge])
+    paths.getOrElse(target, emptyPath).path
   }
 }
 
